7f3cbf9728129a50b2faf36c317712348dde5303
Shaharukh Khan <82607480+Srkskb@users.noreply.github.com>
2022-12-28 18:04:54 +0530

    ch

--- src/app/screens/educator/EducatorResources/EducatorAddResources.js
+++ src/app/screens/educator/EducatorResources/EducatorAddResources.js
@@ -1,47 +1,236 @@
-import { View, Text, ScrollView, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  StatusBar,
+  Image,
+} from "react-native";
 import color from "../../../assets/themes/Color";
 import HeaderBack from "../../../components/header/Header";
-import Input from "../../../components/inputs/Input";
+import InputField from "../../../components/inputs/Input";
+import { AccessLevel } from "../../../components/dropdown";
 import SmallButton from "../../../components/buttons/SmallButton";
+import { useNavigation } from "@react-navigation/native";
+import { myHeadersData } from "../../../api/helper";
+import axios from "axios";
+import { Snackbar } from "react-native-paper";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import * as ImagePicker from "expo-image-picker";
+import { UploadDocument } from "../../../components";
+import mime from 'mime'
 export default function EducatorAddResources({ navigation }) {
+  const [question,setQuestion]=useState("");
+  const [answer ,setAnswer] =useState("");
+  const loginUID = localStorage.getItem("loginUID");
+  const [snackVisibleTrue, setSnackVisibleTrue] = useState(false);
+  const [snackVisibleFalse, setSnackVisibleFalse] = useState(false);
+  const [getMessageTrue, setMessageTrue] = useState();
+  const [getMessageFalse, setMessageFalse] = useState();
+
+  const addFileCabinet = (values) => {
+    console.log(values.docTitle,loginUID,values.description,);
+    const getHeaders = myHeadersData();
+    var data = new FormData();
+    data.append('add_faq', '1');
+    data.append('Question', values.docTitle);
+    data.append('user_id', loginUID);
+    data.append('Answer', values.description);
+  var config = {
+    method: 'post',
+    url: 'https://3dsco.com/3discoapi/studentregistration.php',
+    headers: { 
+      'Accept': 'application/json', 
+      'Content-Type': 'application/x-www-form-urlencoded', 
+      'Cookie': 'PHPSESSID=nquo5j2tsolthnpt2f6hhuuj82'
+    },
+    data : data
+  };
+  
+  axios(config)
+  .then((response)=>{
+    if(response.data.success==1){
+    navigation.navigate("EducatorManageResources")
+  }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+};
   return (
-    <View style={styles.main}>
-      <HeaderBack title={"Add Resources"} 
-       onPress={() => navigation.goBack()}
+<View style={styles.container}>
+      <StatusBar backgroundColor={color.purple} />
+      <HeaderBack
+        title={"Add Picture"}
+        // onPress={() => navigation.navigate("FileCabinet")}
+        onPress={() => navigation.goBack()}
       />
-      <ScrollView style={styles.container}>
-        <Input label={"Question"} placeholder={"Enter Your Question"} />
-        <Input label={"Answer"} placeholder={"Enter Your Answer"} />
-        <View style={styles.button}>
-          <SmallButton
-            title={"Cancel"}
-            color={color.purple}
-            fontFamily={"Montserrat-Medium"}
-          />
-          <SmallButton
-            title="Post"
-            color={color.white}
-            backgroundColor={color.purple}
-            fontFamily={"Montserrat-Bold"}
-            onPress={() => navigation.navigate("EducatorManageResources")}
-          />
-        </View>
-      </ScrollView>
+      <Snackbar
+        visible={snackVisibleTrue}
+        onDismiss={() => setSnackVisibleTrue(false)}
+        action={{ label: "Close" }}
+        theme={{ colors: { accent: "#82027D" } }}
+      >
+        {getMessageTrue}
+      </Snackbar>
+      <Snackbar
+        visible={snackVisibleFalse}
+        onDismiss={() => setSnackVisibleFalse(false)}
+        action={{ label: "Close" }}
+        theme={{ colors: { accent: "red" } }}
+      >
+        {getMessageFalse}
+      </Snackbar>
+      <View style={styles.main}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View>
+            <Formik
+              initialValues={{
+                docTitle: "",
+                description: "",
+              }}
+              validationSchema={Yup.object().shape({
+                docTitle: Yup.string()
+                  .required("Document Title is required")
+                  .min(3, "Document Title must be at least 3 characters")
+                  .max(50, "Document Title cannot be more than 50 characters"),
+                description: Yup.string()
+                  .required("Description is required")
+                  .max(250, "Description cannot be more than 50 characters"),
+              })}
+              onSubmit={(values) => addFileCabinet(values)}
+            >
+              {({
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                values,
+                errors,
+                isValid,
+              }) => (
+                <View>
+                  <InputField
+                    label={"Question"}
+                    placeholder={"Enter Your Question"}
+                    name="title"
+                    onChangeText={handleChange("docTitle")}
+                    onBlur={handleBlur("docTitle")}
+                    value={values.docTitle}
+                    keyboardType="text"
+                  />
+                  {errors.docTitle && (
+                    <Text
+                      style={{ fontSize: 14, color: "red", marginBottom: 10 }}
+                    >
+                      {errors.docTitle}
+                    </Text>
+                  )}
+                  {/* <AccessLevel
+                    required
+                    label={"Access Level"}
+                    onSelect={(selectedItem, index) => {
+                      setAccess(selectedItem);
+                      console.log(selectedItem, index);
+                    }}
+                    value={access}
+                  /> */}
+
+                  {errors.selectedItem && (
+                    <Text
+                      style={{ fontSize: 14, color: "red", marginBottom: 10 }}
+                    >
+                      {errors.selectedItem}
+                    </Text>
+                  )}
+{/*  */}
+                  <InputField
+                    label={"Answer"}
+                    placeholder={"Enter Your Answer"}
+                    name="description"
+                    multiline={true}
+                    numberOfLines={6}
+                    onChangeText={handleChange("description")}
+                    onBlur={handleBlur("description")}
+                    value={values.description}
+                    keyboardType="default"
+                    textAlignVertical="top"
+                  />
+                  {errors.description && (
+                    <Text
+                      style={{ fontSize: 14, color: "red", marginBottom: 10 }}
+                    >
+                      {errors.description}
+                    </Text>
+                  )}
+
+                  <View style={styles.button}>
+                    <SmallButton
+                      title={"Cancel"}
+                      color={color.purple}
+                      fontFamily={"Montserrat-Medium"}
+                    />
+                    <SmallButton
+                      onPress={handleSubmit}
+                      title="Save"
+                      disabled={!isValid}
+                      color={color.white}
+                      backgroundColor={color.purple}
+                      fontFamily={"Montserrat-Bold"}
+                    />
+                  </View>
+                </View>
+              )}
+            </Formik>
+          </View>
+        </ScrollView>
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 15,
-    paddingTop: 20,
+    flex: 1,
+    backgroundColor: color.white,
+  },
+  main: {
+    flex: 1,
+    padding: 20,
+  },
+  headline: {
+    fontFamily: "Montserrat-Regular",
+    color: "#081F32",
+    marginBottom: 20,
+    fontSize: 13,
+  },
+  datalines: {
+    fontFamily: "Montserrat-Bold",
+    fontSize: 13,
+    color: color.black,
+    textAlign: "justify",
+    borderBottomWidth: 1,
+    borderBottomColor: "#DADADA",
+    paddingBottom: 20,
+    marginBottom: 20,
+  },
+  description: {
+    fontFamily: "Montserrat-Medium",
+    fontSize: 13,
+    textAlign: "justify",
   },
   button: {
     flexDirection: "row",
     marginTop: 20,
   },
-  main: {
-    backgroundColor: color.white,
-    flex: 1,
+  uploadImg: {
+    height: 100,
+    width: 100,
+    borderWidth: 2,
+    borderRadius: 8,
+    marginTop: 5,
+  },
+  uploadCon: {
+    textAlign: "center",
   },
 });
