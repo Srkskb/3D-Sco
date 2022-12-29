cf640add6aa3ac8a6d1ce409fd3e6792c8f4291b
Shaharukh Khan <82607480+Srkskb@users.noreply.github.com>
2022-12-29 13:12:28 +0530

    ch

--- src/app/screens/educator/EducatorResources/EducatorEditResources.js
+++ src/app/screens/educator/EducatorResources/EducatorEditResources.js
@@ -1,18 +1,70 @@
 import { View, Text, ScrollView, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import color from "../../../assets/themes/Color";
 import HeaderBack from "../../../components/header/Header";
 import Input from "../../../components/inputs/Input";
 import SmallButton from "../../../components/buttons/SmallButton";
-export default function EducatorEditResources({navigation}) {
+import { myHeadersData } from "../../../api/helper";
+import axios from "axios";
+export default function EducatorEditResources({route,navigation}) {
+ const [answer,setAnswer]=useState("")
+ const [question,setQuestion]=useState("")
+ const loginUID = localStorage.getItem("loginUID");
+ const { eventID, eventIDParam } = route.params; // ! Current Event ID
+ const updateEvent = () => {
+  const myHeaders = myHeadersData();
+  var urlencoded = new FormData();
+  urlencoded.append("update_faq", "1");
+  urlencoded.append("Question", question);
+  urlencoded.append("Answer", answer);
+  urlencoded.append("id", eventID);
+  urlencoded.append("user_id", loginUID);
+  var data = qs.stringify({
+    'update_faq': '1',
+    'Question': question,
+    'Answer': answer,
+    'id': eventID,
+    'user_id': loginUID
+  });
+  var config = {
+    method: 'post',
+    url: 'https://3dsco.com/3discoapi/studentregistration.php',
+    headers: { 
+      'Accept': 'application/json', 
+      'Content-Type': 'application/x-www-form-urlencoded', 
+      'Cookie': 'PHPSESSID=8h8r2aeo9i56iqmko6mnvnfjj2'
+    },
+    data : data
+  };
+  
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+    if(response.data.success==1){
+      navigation.navigate("EducatorManageResources")
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+};
   return (
     <View style={styles.main}>
       <HeaderBack title={"Edit Resources"} 
       onPress={() => navigation.goBack()}
       />
       <ScrollView style={styles.container}>
-    <Input label={"Question"} placeholder={"Enter Your Question"}/>
-        <Input label={"Answer"} placeholder={"Enter Your Answer"}/>
+    <Input label={"Question"} placeholder={"Enter Your Question"}
+      onChangeText={(text) => setQuestion(text)}
+      value={question}
+      keyboardType="text"
+    />
+        <Input label={"Answer"} placeholder={"Enter Your Answer"}
+          onChangeText={(text) => setAnswer(text)}
+          value={answer}
+          keyboardType="text"
+        
+        />
         <View style={styles.button}>
           <SmallButton
             title={"Cancel"}
@@ -24,7 +76,7 @@ export default function EducatorEditResources({navigation}) {
             color={color.white}
             backgroundColor={color.purple}
             fontFamily={"Montserrat-Bold"}
-            onPress={() => navigation.navigate("EducatorManageResources")}
+            onPress={updateEvent}
           />
         </View>
       </ScrollView>
