6b3395bbc6ca4a9bb5993d21884d7db5d97a03d1
Shaharukh Khan <82607480+Srkskb@users.noreply.github.com>
2023-01-18 22:46:30 +0530

    chnag

--- src/app/screens/admin/account/Category/Category.js
+++ src/app/screens/admin/account/Category/Category.js
@@ -1,60 +1,170 @@
-import { View, Text, StyleSheet, ScrollView } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, ScrollView, RefreshControl } from "react-native";
 import color from "../../../../assets/themes/Color";
+import { useNavigation } from "@react-navigation/native";
 import HeaderBack from "../../../../components/header/Header";
+import { myHeadersData } from "../../../../api/helper";
+import { NoDataFound } from "../../../../components";
 import TextWithButton from "../../../../components/TextWithButton";
 import SelectCourse from "../../../../components/admin_required/SelectCourse";
-import Category_Card from "../../../../components/admin_required/Cards/Category_Card";
-
-export default function Category({ navigation }) {
+import FileCabinetCard from "../../../../components/card/FileCabinetCard";
+import { Snackbar } from "react-native-paper";
+export default function Category() {
+  const navigation = useNavigation();
+  const [snackVisibleTrue, setSnackVisibleTrue] = useState(false);
+  const [snackVisibleFalse, setSnackVisibleFalse] = useState(false);
+  const [getMessageTrue, setMessageTrue] = useState();
+  const [getMessageFalse, setMessageFalse] = useState();
   const [selectCourse, setSelectCourse] = useState("");
-
-  const DeleteCatergory=()=>{
-    var data = qs.stringify({
-      'delete_category': '1',
-      'id': '22' 
-    });
-    var config = {
-      method: 'post',
-      url: 'https://3dsco.com/3discoapi/studentregistration.php',
-      headers: { 
-        'Content-Type': 'application/x-www-form-urlencoded', 
-        'Cookie': 'PHPSESSID=hc3kbqpelmbu5cl5em37e2j4j7'
-      },
-      data : data
+  const [fileCabinetData, setFileCabinetData] = useState([]);
+  const [color, changeColor] = useState("red");
+  const [refreshing, setRefreshing] = useState(false);
+  const allLearnerList = () => {
+    const loginUID = localStorage.getItem("loginUID");
+    const myHeaders = myHeadersData();
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      redirect: "follow",
     };
-    
-    axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-    
-  }
+    fetch(
+      `https://3dsco.com/3discoapi/studentregistration.php?course_categories_list=1&catID=${loginUID}`,
+      requestOptions
+    )
+      .then((res) => res.json())
+      .then((result) => setFileCabinetData(result.data))
+      .catch((error) => console.log("error", error));
+  };
+  const deleteEvent = (id) => {
+    const loginUID = localStorage.getItem("loginUID");
+    const myHeaders = myHeadersData();
+    var requestOptions = {
+      method: "DELETE",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+    fetch(
+      `https://3dsco.com/3discoapi/3dicowebservce.php?delete_documents=1&id=${id}&catID=${loginUID}`,
+      requestOptions
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        if (result.success === 1) {
+          setSnackVisibleTrue(true);
+          setMessageTrue(result.message);
+          let temp = [];
+          fileCabinetData.forEach((item) => {
+            if (item.id !== id) temp.push(item);
+          });
+          setFileCabinetData(temp);
+        } else {
+          setSnackVisibleFalse(true);
+          setMessageFalse(result.message);
+        }
+      })
+      .catch((error) => console.log("error", error));
+  };
+  const onRefresh = () => {
+    setRefreshing(true);
+    allLearnerList();
+    setTimeout(() => {
+      changeColor("green");
+      setRefreshing(false);
+    }, 2000);
+  };
+  useEffect(() => {
+    allLearnerList();
+    navigation.addListener("focus", () => allLearnerList());
+  }, []);
+
   return (
-    <View style={{backgroundColor:color.white,flex:1}}>
-      <HeaderBack title={"Category"} onPress={()=>navigation.goBack()}/>
-      <ScrollView style={styles.container}>
-        <TextWithButton label={"+Add"} title={"Categories"} onPress={()=>navigation.navigate("AddCategory")}/>
-        <SelectCourse
+    <View style={styles.container}>
+      <HeaderBack
+        title={"Category"}
+        onPress={() => navigation.goBack()}
+      />
+      <Snackbar
+        visible={snackVisibleTrue}
+        onDismiss={() => setSnackVisibleTrue(false)}
+        action={{ label: "Close" }}
+        theme={{ colors: { accent: "#82027D" } }}
+      >
+        {getMessageTrue}
+      </Snackbar>
+      <Snackbar
+        visible={snackVisibleFalse}
+        onDismiss={() => setSnackVisibleFalse(false)}
+        action={{ label: "Close" }}
+        theme={{ colors: { accent: "red" } }}
+      >
+        {getMessageFalse}
+      </Snackbar>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+        style={{ paddingHorizontal: 10 }}
+      >
+        <TextWithButton
+          title={"Course Category"}
+          label={"+Add"}
+          onPress={() => navigation.navigate("AddCategory")}
+        />
+          <SelectCourse
           label={"Select Course"}
           onSelect={(selectedItem, index) => {
             setSelectCourse(selectedItem);
           }}
         />
-        
-        <Category_Card
-          title={"Backup"}
-          editPress={() => navigation.navigate("EditCategory")}
-        />
+        <View style={{ paddingHorizontal: 10 }}>
+          {fileCabinetData === undefined ? (
+            <>
+              <NoDataFound />
+            </>
+          ) : (
+            <>
+              {fileCabinetData.map((list, index) => (
+                <FileCabinetCard
+                  title={list.name}
+                  onPressEdit={() =>
+                    navigation.navigate("EditCategory", {
+                      title: list.name,
+                    })
+                  }
+                  removePress={() => deleteEvent(list.id)}
+                />
+              ))}
+            </>
+          )}
+        </View>
       </ScrollView>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 15,
+    backgroundColor: color.white,
+    flex: 1,
+    textAlign: "left",
+  },
+  head: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 5,
+    alignItems: "center",
+    height: 40,
+    paddingHorizontal: 10,
+  },
+  manage: {
+    fontFamily: "Montserrat-Medium",
+    fontSize: 14,
+    color: color.dark_gray,
+  },
+  title: {
+    fontFamily: "Montserrat-Bold",
+    fontSize: 14,
+    color: color.black,
+    textTransform: "uppercase",
   },
 });
