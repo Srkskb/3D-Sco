aad43a3563b0eabcd0489f4663ab59b66835af45
Shaharukh Khan <82607480+Srkskb@users.noreply.github.com>
2023-01-20 13:24:53 +0530

    cg

--- src/app/screens/admin/account/Forums/AddForum.js
+++ src/app/screens/admin/account/Forums/AddForum.js
@@ -1,73 +1,181 @@
 import { View, Text, ScrollView, StyleSheet } from "react-native";
-import React from "react";
+import React ,{useState} from "react";
 import color from "../../../../assets/themes/Color";
 import HeaderBack from "../../../../components/header/Header";
-import Input from "../../../../components/inputs/Input";
+import InputField from "../../../../components/inputs/Input";
 import SmallButton from "../../../../components/buttons/SmallButton";
 import SelectCourse from "../../../../components/admin_required/SelectCourse";
-import { UploadDocument } from "../../../../components";
 import ActiveStatus from "../../../../components/dropdown/ActiveStatus";
+import { UploadDocument } from "../../../../components";
+import axios from "axios";
+import * as Yup from "yup";
+import { Formik } from "formik";
+import qs from 'qs';
 export default function AddForum({navigation}) {
-  const AddForum=()=>{
+  const [title, setTitle]=useState("")
+  const[description,setDescription]=useState("")
+  const[status,SetStatus]=useState("Select Status")
+  const[course,setCourse]=useState("Select Course")
+  const loginUID = localStorage.getItem("loginUID");
+  const AddForum=(values)=>{
+    console.log(values.docTitle,course,loginUID,values.description,);
     var data = qs.stringify({
       'add_courses_forum': '1',
-      'user_id': '176',
-      'admin_id': '176',
-      'forum_title': 'Reply',
-      'Description': 'Distance education and virtual connection in cyber...',
-      'course_id': '5',
-      'topic_id': '3' 
+      'user_id': loginUID,
+      'admin_id': "",
+      'forum_title': values.title,
+      'Description': values.description,
+      'course_id': course,
+      'topic_id': status
     });
-    var config = {
-      method: 'post',
-      url: 'https://3dsco.com/3discoapi/studentregistration.php',
-      headers: { 
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      data : data
-    };
-    
-    axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-    
+var config = {
+  method: 'post',
+  url: 'https://3dsco.com/3discoapi/studentregistration.php',
+  headers: { 
+    'Cookie': 'PHPSESSID=hc3kbqpelmbu5cl5em37e2j4j7', 
+    data
+  },
+  data : data
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+  navigation.navigate("Forum")
+})
+.catch(function (error) {
+  console.log(error);
+});
   }
   return (
     <View style={{backgroundColor:color.white,flex:1}}>
       <HeaderBack title={"Add Forum"} onPress={()=>navigation.goBack()} />
-      <ScrollView style={styles.container}>
-        <SelectCourse
+      <View style={styles.main}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View>
+            <Formik
+              initialValues={{
+                docTitle: "",
+                description: "",
+              }}
+              validationSchema={Yup.object().shape({
+                docTitle: Yup.string()
+                  .required("Document Title is required")
+                  .min(3, "Document Title must be at least 3 characters")
+                  .max(50, "Document Title cannot be more than 50 characters"),
+                description: Yup.string()
+                  .required("Description is required")
+                  .min(20, "Description must be at least 20 characters")
+                  .max(250, "Description cannot be more than 50 characters"),
+              })}
+              onSubmit={(values) => AddForum(values)}
+            >
+              {({
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                values,
+                errors,
+                isValid,
+              }) => (
+                <View>
+
+<SelectCourse
           label={"Select Course"}
-          onSelect={(selectedItem, index) => {}}
+          onSelect={(selectedItem, index) => {
+            setCourse(selectedItem)
+            console.log(selectedItem, index);
+            
+          }}
+          value={course}
         />
-        <Input label={"Forum Title"} placeholder={"Forum Title"} />
-        <ActiveStatus  onSelect={(selectedItem, index) => {}}/>
-        <Input
-          label={"Description"}
-          placeholder={"Description"}
-          multiline={true}
-          numberOfLines={5}
-          textAlignVertical={"top"}
-        />
-        {/* <UploadDocument type={"File"}/> */}
-        <View style={styles.button}>
-          <SmallButton
-            title={"Cancel"}
-            color={color.purple}
-            fontFamily={"Montserrat-Medium"}
-          />
-          <SmallButton
-            title="Save"
-            color={color.white}
-            backgroundColor={color.purple}
-            fontFamily={"Montserrat-Bold"}
-          />
-        </View>
-      </ScrollView>
+                  <InputField
+                    label={"Forum Title"}
+                    placeholder={"Forum Title"}
+                    name="title"
+                    onChangeText={handleChange("docTitle")}
+                    onBlur={handleBlur("docTitle")}
+                    value={values.docTitle}
+                    keyboardType="text"
+                  />
+                  {errors.docTitle && (
+                    <Text
+                      style={{ fontSize: 14, color: "red", marginBottom: 10 }}
+                    >
+                      {errors.docTitle}
+                    </Text>
+                  )}
+                  {/* <AccessLevel
+                    required
+                    label={"Access Level"}
+                    onSelect={(selectedItem, index) => {
+                      setAccess(selectedItem);
+                      console.log(selectedItem, index);
+                    }}
+                    value={access}
+                  /> */}
+       
+  <ActiveStatus  onSelect={(selectedItem, index) => {
+
+    SetStatus(selectedItem)
+    console.log(selectedItem, index);
+  }}/>
+                  {errors.selectedItem && (
+                    <Text
+                      style={{ fontSize: 14, color: "red", marginBottom: 10 }}
+                    >
+                      {errors.selectedItem}
+                    </Text>
+                  )}
+
+                  {/* <UploadDocument onPress={pickImage} /> */}
+                  {/* <View style={styles.uploadCon}>
+                    {image && (
+                      <Image source={{ uri: image }} style={styles.uploadImg} />
+                    )}
+                  </View> */}
+                  <InputField
+                    label={"Description"}
+                    placeholder={"Description"}
+                    name="description"
+                    multiline={true}
+                    numberOfLines={6}
+                    onChangeText={handleChange("description")}
+                    onBlur={handleBlur("description")}
+                    value={values.description}
+                    keyboardType="default"
+                    textAlignVertical="top"
+                  />
+                  {errors.description && (
+                    <Text
+                      style={{ fontSize: 14, color: "red", marginBottom: 10 }}
+                    >
+                      {errors.description}
+                    </Text>
+                  )}
+
+                  <View style={styles.button}>
+                    <SmallButton
+                      title={"Cancel"}
+                      color={color.purple}
+                      fontFamily={"Montserrat-Medium"}
+                      onPress={()=>navigation.navigate("Forum")}
+                    />
+                    <SmallButton
+                      onPress={handleSubmit}
+                      title="Save"
+                      disabled={!isValid}
+                      color={color.white}
+                      backgroundColor={color.purple}
+                      fontFamily={"Montserrat-Bold"}
+                    />
+                  </View>
+                </View>
+              )}
+            </Formik>
+          </View>
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -79,5 +187,44 @@ const styles = StyleSheet.create({
   button: {
     flexDirection: "row",
     marginTop: 20,
+  },
+  main: {
+    flex: 1,
+    padding: 20,
+  },
+  headline: {
+    fontFamily: "Montserrat-Regular",
+    color: "#081F32",
+    marginBottom: 20,
+    fontSize: 13,
+  },
+  datalines: {
+    fontFamily: "Montserrat-Bold",
+    fontSize: 13,
+    color: color.black,
+    textAlign: "justify",
+    borderBottomWidth: 1,
+    borderBottomColor: "#DADADA",
+    paddingBottom: 20,
+    marginBottom: 20,
+  },
+  description: {
+    fontFamily: "Montserrat-Medium",
+    fontSize: 13,
+    textAlign: "justify",
+  },
+  button: {
+    flexDirection: "row",
+    marginTop: 20,
+  },
+  uploadImg: {
+    height: 100,
+    width: 100,
+    borderWidth: 2,
+    borderRadius: 8,
+    marginTop: 5,
+  },
+  uploadCon: {
+    textAlign: "center",
   },
 });
